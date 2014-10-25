8f5861555b536fc441cf372c19c7a7aeb714dbdd
magnushg <magnus.h.green@gmail.com>
2014-10-25 14:35:14 +0200

    Add Firebase simple message

--- superCtrl.js
+++ superCtrl.js
@@ -1,5 +1,11 @@
-angular.module('superguide', [])
-  .controller('superController', ['$scope', function($scope) {
-    $scope.message = "Hello world";
+angular.module('superguide', ['firebase'])
+  .controller('superController', ['$scope', '$firebase', function($scope, $firebase) {
+    var ref = new Firebase("https://superguide.firebaseio.com/Position");
+    var sync = $firebase(ref);
+
+    $scope.message = sync.$asObject();
+
     $scope.position = {lat: 58.77, lng: 10.77};
+
+
   }]);
