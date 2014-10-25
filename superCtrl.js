a1d28c31eb6cadac740d67fd41b56ac88d4ee1f6
magnushg <magnus.h.green@gmail.com>
2014-10-25 22:42:30 +0200

    Add chat message endpoint

--- superCtrl.js
+++ superCtrl.js
@@ -2,17 +2,18 @@ angular.module('superguide', ['firebase'])
   .controller('superController', ['$scope', '$firebase', function($scope, $firebase) {
     var ref = new Firebase("https://superguide.firebaseio.com/Position/");
     var broadcastref = new Firebase("https://superguide.firebaseio.com/BroadcastMessage");
+    var chatref = new Firebase("https://superguide.firebaseio.com/UserMessages");
+
     var sync = $firebase(ref);
     var messageSync = $firebase(broadcastref);
-
+    var chatSync = $firebase(chatref);
 
 
     //var syncObject = sync.$asObject();
     $scope.users = sync.$asArray();
     //syncObject.$bindTo($scope, "userData");
     var message = messageSync.$asObject();
+    $scope.chat = chatSync.$asArray();
     message.$bindTo($scope, "message");
     $scope.position = {lat: 58.77, lng: 10.77};
-
-
   }]);
