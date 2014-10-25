9c30ef2a15828c7b0e297dab722dc1524c8dcf76
magnushg <magnus.h.green@gmail.com>
2014-10-25 12:36:32 +0200

    add angular

---
+++ superCtrl.js
@@ -0,0 +1,5 @@
+angular.module('superguide', [])
+  .controller('superController', ['$scope', function($scope) {
+    $scope.message = "Hello world";
+    $scope.position = {lat: 58.77, lng: 10.77};
+  }]);
