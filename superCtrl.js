7d745dfecb1d4d05707b5e6be435c63ce59c0d79
magnushg <magnus.h.green@gmail.com>
2014-10-25 14:06:27 +0200

    Push test 2

--- superCtrl.js
+++ superCtrl.js
@@ -1,5 +1,5 @@
 angular.module('superguide', [])
   .controller('superController', ['$scope', function($scope) {
     $scope.message = "Hello world";
-    $scope.position = {lat: 58.77, lng: 10.77}; 
+    $scope.position = {lat: 58.77, lng: 10.77};
   }]);
